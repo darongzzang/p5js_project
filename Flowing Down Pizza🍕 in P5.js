092908061cc11299f3446383f95c9abfb9a94510
darongzzang <101727090+darongzzang@users.noreply.github.com>
2023-05-02 22:13:31 +0900

    Update and rename flowing down pizza in P5.js to Flowing Down Pizza🍕 in P5.js

---
+++ Flowing Down Pizza🍕 in P5.js
@@ -0,0 +1,159 @@
+
+let y = 0;
+
+function setup() {
+  createCanvas(800, 800 );
+}
+
+function draw() {
+  background('#F1F571');
+  
+//   //box
+//   fill('#C4A67D');
+//   quad(120, 480, 250, 420, 250, 500, 120, 560);
+//   quad(550, 420, 680, 480, 680, 560, 550, 500);
+//   fill('#AB916D');
+//   quad(250, 420, 550, 420, 550, 500, 250, 500);
+//   quad(250, 500, 550, 500, 370, 560, 120, 560);
+  
+  
+  
+  //plain 
+  
+  stroke(194, 150, 113);
+  strokeWeight(35);
+  line(400, 550, 630, 550);
+  arc(410, 560, 500, 150, radians(60), radians(178));
+  
+ 
+  
+  strokeWeight(20);
+  stroke('#BF4904');
+  fill('#EB9B3A');
+  arc(400, 550, 500, 150, radians(45), radians(360));
+  
+  
+  
+  stroke(194, 150, 113, 80);
+  strokeWeight(3);
+  line(160, 550, 640, 550);
+  line(330, 480, 470, 620);
+  line(550, 500, 260, 600);
+  
+ 
+  strokeWeight(20);
+  stroke('#BF4904');
+  noFill();
+  arc(400, 550, 500, 150, radians(55), radians(362));
+  
+  //cheese
+  fill('#FCA73F')
+  noStroke();
+  rect(440, 450, 20, 90);
+  circle(450, 535+y, 20);
+  rect(500, 450, 20, 80);
+  circle(510, 525+y, 20);
+  rect(540, 450, 20, 100);
+  circle(550, 545+y, 20);
+  
+  
+  //a piece of pizza
+  stroke(194, 150, 113);
+  strokeWeight(20);
+  line(400, 460, 482,522);
+  stroke('#BF4904');
+  fill('#FCA73F');
+  strokeWeight(20);
+  arc(400, 450, 500, 150, radians(0), radians(35));
+  
+  fill('#FCA73F')
+  noStroke();
+  rect(405, 455, 20, 110);
+  circle(415, 560+y, 20);
+  rect(470, 475, 20, 70);
+  circle(480, 540+y, 20);
+  
+  
+  
+  
+
+ 
+  
+  //topping
+  
+  fill('#BF1304');
+  noStroke();
+  // ellipse(400, 500, 30, 20);
+  // ellipse(460, 510, 30, 20);
+  // ellipse(430, 530, 25, 15);
+  
+  
+  // ellipse(550, 520, 30, 20);
+  // ellipse(500, 540, 25, 15);
+  ellipse(320, 510, 30, 20);
+  ellipse(250, 530, 30, 20);
+  ellipse(340, 540, 25, 15);
+  
+  ellipse(210, 570, 30, 20);
+  ellipse(280, 580, 30, 20);
+  
+
+  ellipse(390, 580, 30, 20);
+  ellipse(330, 600, 30, 20);
+  ellipse(410, 610, 25, 15);
+  
+  //piece topping
+  ellipse(570, 470, 30, 20);
+  ellipse(510, 490, 30, 20);
+  ellipse(480, 470, 25, 20);
+  
+  
+  fill('#F7D896');
+  translate(500, 530);
+  rotate(QUARTER_PI);
+  rectMode(CENTER);
+  rect(0, 0, 1, 1);
+  translate(70, -50);
+  rect(0, 0, 1, 1);
+  
+  translate(-180, 120);
+  rotate(radians(75));
+  rect(0, 0, 20, 10);
+  translate(70, 70);
+  rect(0, 0, 20, 10);
+  translate(40, -40);
+  rect(0, 0, 20, 10);
+  
+  translate(-50, -70);
+  rotate(radians(130));
+  rect(0, 0, 20, 10);
+  translate(90, -50);
+  rect(0, 0, 20, 10);
+  translate(-40, 110);
+  rect(0, 0, 1, 1);
+  translate(30, -190);
+  rect(0, 0, 20, 10);
+  translate(-40, 350);
+  rect(0, 0, 20, 10);
+  translate(-10, -60);
+  rect(0, 0, 20,10);
+  rectMode(CORNER);
+  
+  y+=0.1;
+  if(y>10){
+    y = 0;
+    console = y;
+  
+// tries
+//   //피자박스 앞쪽
+//   fill('#DE1605');
+//   quad(120, 480, 280, 600, 280, 690, 120, 570 );
+    //왜.. 안될까? 왜 피자 앞에 안그려지지
+  
+  
+  // //피자 조각
+  // fill('#F5A23D');
+  // arc(600, 450, 500, 150, radians(0), radians(55));
+ 
+}
+}
--- flowing down pizza in P5.js
+++
@@ -1,1 +0,0 @@
-https://editor.p5js.org/darongzzang/sketches/Oz4WeHupK
